2f7155307bb18984cbc6eecef6331a07fafc4f57
ADEP-123 <andep210399@gmail.com>
2024-04-08 19:08:30 -0500

    feat: :sparkles: main index simulating app

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+import getCountriesorCities from "./modules/getCountries.js";
+import getInfo from "./modules/getInfo.js";
+import Variables from "./variables.js";
+
+const variables = new Variables;
+
+// variables.setURL("bogota", "CO")
+// getInfo(variables.APIurl)
+getCountriesorCities(variables.paisesURL,"country")
+variables.setCiudadesUrl("co");
+getCountriesorCities(variables.ciudadesURL,"cities")
+
